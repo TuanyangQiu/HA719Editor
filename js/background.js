1b6a83529365d02ebd79a0c747ff1159abb1bc28
TuanyangQiu <qiutuanyang@gmail.com>
2023-07-23 20:45:42 +1200

    display the main page in a new tab, so user can easily operate it

--- js/background.js
+++ js/background.js
@@ -7,12 +7,26 @@ chrome.contextMenus.create({
 },
   function() { });
 
-  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
-    if (info.menuItemId === "getLinkInfo") {
-      console.log('clicked getLinkInfo');
+  if (info.menuItemId === "getLinkInfo") {
+    console.log('clicked getLinkInfo');
       
-      
-      chrome.tabs.sendMessage(tab.id, { action: "getLinkInfo" });
-    }
-  });
+    
+    chrome.tabs.sendMessage(tab.id, { action: "getLinkInfo" });
+  }
+});
+
+//Listen the click event on the extension icon,
+//then display the page in a new tab
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+  chrome.windows.create({
+    url: chrome.runtime.getURL("../html/mainPage.html"),
+    type: "popup"//hide tool bar
+  },
+    function (event) {
+      console.log('mainpage has been opened!');
+    });
+});
+
