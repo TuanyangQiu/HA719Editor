a587e567bfb45eaa2d4ef2bf587c9f5f678f9d07
TuanyangQiu <qiutuanyang@gmail.com>
2023-07-26 15:20:15 +1200

    popup a diaglogue for style selection, so users can select their interested styles

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,14 +1,22 @@
 console.log("hello main page.js");
 
 chrome.runtime.onMessage.addListener((message) => {
-  const { action, style, text } = message;
+  const { action, style, text, expectedClickInfo } = message;
   if (action === "send-to-popup") {
-    console.log("popup.onMessage");
 
-    const el = document.createElement("div");
-    //el.style = style;
-    el.textContent = text;
-    document.body.appendChild(el);
+    //Popup a modal here so user can select what styles they care about
+    $('#SelectionBoxContent').html('<p>good done! </p>');
+    $('#stylesSelectionBox').modal('show');
 
+    //Saving styles selection
+    $('#saveSelectionChanges').click(function () {
+      $('#stylesSelectionBox').modal('hide');
+    });
+
+    //Discard styles selection
+    $('#discardSelectionChages').click(function () {
+      $('#stylesSelectionBox').modal('hide');
+    });
   }
 });
+
